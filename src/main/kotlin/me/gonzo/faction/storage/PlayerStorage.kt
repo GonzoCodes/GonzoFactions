5438decd3cc40c56f078ac7ff4a8e9b12264add6
Kotlin <joaoabencoadojv@gmail.com>
2019-01-31 23:37:08 -0200

    JSON storage system

--- src/main/kotlin/me/gonzo/faction/storage/PlayerStorage.kt
+++ src/main/kotlin/me/gonzo/faction/storage/PlayerStorage.kt
@@ -1,8 +1,47 @@
 package me.gonzo.faction.storage
 
-class PlayerStorage : Storage() {
+import com.google.gson.*
+import me.gonzo.faction.generics.toList
+import me.gonzo.faction.types.players.Player
+import java.lang.reflect.Type
 
-    override fun makeTable() {
+class PlayerStorage : JsonSerializer<Player>, JsonDeserializer<Player> {
 
+    override fun serialize(player: Player, type : Type, context: JsonSerializationContext): JsonElement {
+        val obj = JsonObject()
+        obj.addProperty("name", player.name)
+        obj.addProperty("faction", player.faction)
+        obj.addProperty("last", player.last)
+
+        val jsoncombat = JsonArray()
+        val jsondeaths = JsonArray()
+        val jsonprimite = JsonArray()
+
+
+        for (f in player.combat.slaughter.toList()) {
+            val slaughter = JsonPrimitive(f)
+            jsoncombat.add(slaughter)
+        }
+
+        for (f in player.combat.deaths.toList()) {
+            val deaths = JsonPrimitive(f)
+            jsondeaths.add(deaths)
+        }
+
+        for (f in player.power.power.toList()) {
+            val power = JsonPrimitive(f)
+            jsonprimite.add(power)
+        }
+
+        obj.add("combate", jsoncombat)
+        obj.add("combate", jsoncombat)
+        obj.add("poder", jsonprimite)
+
+
+        return obj
+    }
+
+    override fun deserialize(player: JsonElement, type: Type, context: JsonDeserializationContext): Player? {
+        return null
     }
 }
--- src/main/kotlin/me/gonzo/faction/storage/Storage.kt
+++ src/main/kotlin/me/gonzo/faction/storage/Storage.kt
@@ -1,7 +1,30 @@
 package me.gonzo.faction.storage
 
-abstract class Storage {
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import me.gonzo.faction.types.players.Player
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
-    abstract fun makeTable()
+object Storage {
 
+    fun readPlayer(file: File) {
+        val gson = Gson()
+        file.listFiles().filter {
+            it.extension == "json"
+        }.forEach {
+            gson.fromJson(it.bufferedReader(), Player::class.java)
+        }
+    }
+
+    @Throws(IOException::class)
+    fun whitePlayer(player : Player) {
+
+        val gson = GsonBuilder().registerTypeAdapter(Player::class.java, PlayerStorage()).setPrettyPrinting().create()
+
+        val writer = FileWriter(player.name + ".json")
+        writer.write(gson.toJson(player))
+        writer.close()
+    }
 }
